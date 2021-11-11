4d5b20f180a2f2838c5cad93feb99940e8d7642b
Bagansio <alexmoaleal.5@gmail.com>
2021-11-11 18:59:27 +0100

    [ENHANCEMENT] Created Attribute Class and ItemType Enum

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+‚
+Rsrc/main/java/com/recommender/recommenderapp/Domain/DataControllers/ICtrlItem.java,f\2\f2a297a32e47bf435708a0ddc881336be72db2ba
+t
+Dsrc/main/java/com/recommender/recommenderapp/Domain/Models/User.java,a\3\a3a274dcf3198029af6e30b9219da4b3a03adcfe
--- src/main/java/com/recommender/recommenderapp/Domain/Models/Item.java
+++ src/main/java/com/recommender/recommenderapp/Domain/Models/Item.java
@@ -1,4 +1,54 @@
 package com.recommender.recommenderapp.Domain.Models;
 
+
+import com.recommender.recommenderapp.Domain.Utils.ItemTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Item {
+
+    private String id;
+    private String title;
+    private ItemTypes type;
+
+    private List<Attribute> stringAttributes;
+    private List<Attribute> intAttributes;
+    private List<Attribute> floatAttributes;
+    private List<Attribute> setAttributes;
+
+    public Item(String id, String title, ItemTypes type, Map<String,String> strings, Map<String,List<String>> arrayAttributes) {
+        this.id = id;
+        this.title = title;
+        this.type = type;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        if (title.equals(null)){
+            return "NoTitle";
+        }
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ItemTypes getType() {
+        return type;
+    }
+
+    public void setType(ItemTypes type) {
+        this.type = type;
+    }
 }
