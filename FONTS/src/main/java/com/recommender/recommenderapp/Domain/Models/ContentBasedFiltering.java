1652e182eab52c3e74cff4cb27a6fe412513d62b
Bagansio <alexmoaleal.5@gmail.com>
2021-11-21 22:20:48 +0100

    Created a readme for Makefile

--- FONTS/src/main/java/com/recommender/recommenderapp/Domain/Models/ContentBasedFiltering.java
+++ FONTS/src/main/java/com/recommender/recommenderapp/Domain/Models/ContentBasedFiltering.java
@@ -252,7 +252,7 @@ public class ContentBasedFiltering extends Algorithm{
     }
 
     /**
-     *                          Gived neighbourhood and knownItemRating return the items with best calculated rating.
+     *                          Given a neighbourhood and a knownItemRating return the items with best calculated rating.
      * @param neighbourhood     A map with key (itemKnownId), and Value ( Map with key (itemUnknownId) and value
      *                          (Distance between itemKnown and itemUnknown))
      * @param knownItemsRating  Rates of the items known | Map - key (itemKnownId) value(rate)
