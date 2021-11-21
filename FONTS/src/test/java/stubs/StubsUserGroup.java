10826ee45aa9bc00692dd7ae9b988fd17b62d0e2
Bagansio <alexmoaleal.5@gmail.com>
2021-11-21 23:22:45 +0100

    Added A 'functional' Main

--- FONTS/src/test/java/stubs/StubsUserGroup.java
+++ FONTS/src/test/java/stubs/StubsUserGroup.java
@@ -1,9 +1,20 @@
 package stubs;
 
+import com.recommender.recommenderapp.Domain.Models.User;
 import com.recommender.recommenderapp.Domain.Models.UserGroup;
 
 public class StubsUserGroup {
     public UserGroup getStubUserGroup() {
         return new UserGroup("1");
     }
+    public User getStubUser() {
+        return new User("test");
+    }
+    public UserGroup getStubUserGroupWithUser() {
+        UserGroup ug = new UserGroup("1");
+        User user = new User("test");
+        ug.addUser(user);
+        return ug;
+    }
+
 }
